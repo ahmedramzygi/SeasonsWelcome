eefd1598b6284b9f201b598574a3d46c0a072446
Ahmed Essam <aeramzy9@gmail.com>
2020-09-23 02:03:32 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
+   
+        state={lat:null,error:null};
+       
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+   componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+        position=>this.setState({lat:position.coords.latitude}),
+
+        err=>this.setState({error:err.message})
+    );
+     
+   }
+   componentDidUpdate(){
+     
+   }
+   renderContent(){
+    if(this.state.error && !this.state.lat){
+        return <div> Error:{this.state.error}</div>
+        }
+        if( !this.state.error && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />
+        
+        }
+        return <Spinner message="Please accept location request"/>;
+   }
+
+    //React says that we have to define render
+    render(){
+        //Conditional Rendering
+               return(
+               <div className='border red'>{this.renderContent()}</div>
+               )
+                
+            }
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'))
